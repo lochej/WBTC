04e9e8b556c416a1c56ec33033d5e27241a8a1c8
lochej <lochejeremy@gmail.com>
2020-01-14 21:58:28 +0100

    Added LED fully-on trigger when powering down the system

--- src/wbtc_manager/GPIO/button_poweroff.sh
+++ src/wbtc_manager/GPIO/button_poweroff.sh
@@ -2,4 +2,4 @@
 
 
 #Wait for falling edge on gpiochip 5 button 0 KEY2
-./gpio_wait_for_event.sh 1 25 0 && poweroff
+./gpio_wait_for_event.sh 1 25 0 && echo default-on > /sys/class/leds/hps_led0/trigger && sudo poweroff
